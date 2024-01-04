3caa88b5c3b6860b4fac2df272e2d806fd95545e
Jonathan von Rad <jonathan.vonrad@gmail.com>
2024-01-04 13:39:33 +0100

    completed eval-function

--- student_agents/template.py
+++ student_agents/template.py
@@ -1,5 +1,7 @@
 import random
 
+from ..ChessEngine import GameState
+
 
 class Agent:
     def __init__(self):
@@ -13,6 +15,7 @@ class Agent:
         self.globalBestScore = None
         self.nextMoveScore = None
         self.is_turn = False
+        self.color = None
 
         self.piece_tables = {
             'p': [
@@ -76,7 +79,6 @@ class Agent:
         self.move_queue = outer_queue
 
     def findBestMove(self, gs):
-        self.is_turn = True
         """
         Parameters
         ----------
@@ -87,6 +89,15 @@ class Agent:
         none
 
         """
+        # Now it's our turn
+        self.turn = True
+        # Set Color initially
+        if self.color == None:
+            if gs.whiteToMove == True:
+                self.color = 'White'
+            else:
+                self.color = 'Black'
+
         validMoves = gs.getValidMoves()
         pass
 
@@ -101,6 +112,7 @@ class Agent:
         Score: Integer
 
         """
+        # check for checkmate / stalemate / draw
         if gs.checkMate:
             if self.turn:
                 return -9999
@@ -112,7 +124,33 @@ class Agent:
         if gs.draw:
             return 0
 
+        # number of pieces
+        piece_counts = self.count_chess_pieces(gs.board)
+
+        # material and piece_scores
+        scores = self.calculateScores(piece_counts, gs)
+
+        # sum of all scores
+        eval = scores['material'] + scores['pawn'] + scores['knight'] + \
+            scores['bishop'] + scores['queen'] + scores['king']
+
+        # favorable position for white = unfavorable position for black
+        if gs.whiteToMove:
+            return eval
+        else:
+            return -eval
+
     def count_chess_pieces(self, board):
+        """
+        Counts the number of each chess piece on the board.
+
+        Args:
+            board (list of str): List representing the current state of the chess board.
+
+        Returns:
+            dict: A dictionary containing counts for each chess piece, categorized by color and type.
+                  Keys are strings representing piece names, and values are the respective counts.
+        """
         piece_counts = {
             'wK': 0, 'wQ': 0, 'wB': 0, 'wN': 0, 'wp': 0,
             'bK': 0, 'bQ': 0, 'bB': 0, 'bN': 0, 'bp': 0,
@@ -144,50 +182,96 @@ class Agent:
 
         return piece_counts
 
-    def calculateScores(piece_counts, gs):
+    def calculateScores(self, piece_counts, gs):
+        """
+        Calculates scores based on the counts of chess pieces.
+
+        Args:
+            piece_counts (dict): A dictionary containing counts for each chess piece,
+                                 categorized by color and type.
+                                 Keys are strings representing piece names, and values are the respective counts.
+            gs (GameState): The current state of the chess game.
+        Returns:
+            dict: A dictionary containing various scores, including material and individual piece scores.
+                  Keys are strings representing score types, and values are the respective scores.
+        """
+
+        # material score
         material = 100 * (piece_counts['wP'] - piece_counts['bP']) + \
             320 * (piece_counts['wN'] - piece_counts['bN']) + \
             330 * (piece_counts['wB'] - piece_counts['bB']) + \
             900 * (piece_counts['wQ'] - piece_counts['bQ'])
 
-        pawnsq = sum([pawntable[i]
-                     for i in board.pieces(chess.PAWN, chess.WHITE)])
+        # individual pieces score
+        pawn_score = self.calculate_piece_value(
+            gs.board, 'wp') - self.calculate_piece_value(gs.board, 'bp')
+        knight_score = self.calculate_piece_value(
+            gs.board, 'wN') - self.calculate_piece_value(gs.board, 'bN')
+        bishop_score = self.calculate_piece_value(
+            gs.board, 'wB') - self.calculate_piece_value(gs.board, 'bB')
+        queen_score = self.calculate_piece_value(
+            gs.board, 'wQ') - self.calculate_piece_value(gs.board, 'bQ')
+        king_score = self.calculate_piece_value(
+            gs.board, 'wK') - self.calculate_piece_value(gs.board, 'bK')
+
+        # Return a dictionary containing the scores
+        scores = {
+            'material': material,
+            'pawn': pawn_score,
+            'knight': knight_score,
+            'bishop': bishop_score,
+            'queen': queen_score,
+            'king': king_score
+        }
+
+        return scores
+
+    def square_mirror(self, index):
+        """
+        Mirrors the given index on a 6x6 chess board for calculating piece scores of white chess pieces.
+
+        Args:
+            index (int): The index of the chess piece in the gs.board array.
+
+        Returns:
+            int: The mirrored index of the chess piece, effectively switching from black to white or vice versa.
+        """
+        row, col = divmod(index, 6)
+        mirrored_index = col + (5 - row) * 6
+        return mirrored_index
+
+    def calculate_piece_value(self, board, piece_name):
+        """
+        Sums up individual piece scores of a chess piece.
+
+        Args:
+            board (List of str): String Array representing Chess Board
+            piece_name (str): Name of chess piece eg. 'bp', 'bB', ...
+
+        Returns:
+            int: Summed up score values for chess piece.
+        """
+
+        # extract type of chesspiece ('n', 'b', etc.)
+        piece_type = piece_name[1].lower()
+        piece_color = piece_name[0]
+        if piece_color == 'b':
+            piece_indices = [i for i, piece in enumerate(
+                board) if piece == piece_name]
+        else:
+            piece_indices = [self.square_mirror(i) for i, piece in enumerate(
+                board) if piece == piece_name]
+
+        piece_value = sum(
+            self.piece_tables[piece_type][i] for i in piece_indices)
+
+        return piece_value
 
 
-board = ['bN', 'bB', 'bQ', 'bK', 'bB', 'bN',
-         'bp', 'bp', 'bp', 'bp', 'bp', 'bp',
-         '--', '--', '--', '--', '--', '--',
-         '--', '--', '--', '--', '--', '--',
-         'wp', 'wp', 'wp', 'wp', 'wp', 'wp',
-         'wN', 'wB', 'wQ', 'wK', 'wB', 'wN']
+agent = Agent()
 
-pawntable = [
-        0, 0, 0, 0, 0, 0,
-        5, 10, -20, -20, 10, 5,
-        5, 10, 20, 20, 10, 5,
-        0, 0, 10, 10, 0, 0,
-        10, 20, 30, 30, 20, 10,
-        50, 50, 50, 50, 50, 50
-    ]
+board = GameState()
 
-piece_values = Agent()
+eval = agent.evaluatePosition(board)
 
-def calculate_piece_value(board, piece_name):
-
-    piece_type = piece_name[1].lower()  # Extrahiere den Typ der Schachfigur ('n', 'b', usw.)
-    piece_indices = [i for i, piece in enumerate(board) if piece == piece_name]
-
-    piece_value = sum(piece_values.piece_tables[piece_type][i] for i in piece_indices)
-
-    return piece_value
-
-print(calculate_piece_value(board, 'bB'))
-
-## teste calculate_piece_value und schaue ob es für beide seiten passt
-
-
-## die funktion müsste theoretisch den index für die andere seite angeben
-def square_mirror(index):
-    row, col = divmod(index, 6)
-    mirrored_index = col + (5 - row) * 6
-    return mirrored_index
+print(eval)
